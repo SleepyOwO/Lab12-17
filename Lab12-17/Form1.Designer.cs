076fb1c11c7b2a467c72ee0946a87a49012b4712
Sleepy <fostershinigami@gmail.com>
2025-02-18 23:26:35 +0300

    12-13 accepted

--- Lab12-17/Form1.Designer.cs
+++ Lab12-17/Form1.Designer.cs
@@ -27,8 +27,7 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
+        private void InitializeComponent() {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -42,6 +41,7 @@
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sorting = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -56,7 +56,7 @@
             this.panel1.Controls.Add(this.exit_btn);
             this.panel1.Location = new System.Drawing.Point(0, 314);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(801, 137);
+            this.panel1.Size = new System.Drawing.Size(833, 137);
             this.panel1.TabIndex = 0;
             // 
             // num_label
@@ -89,6 +89,7 @@
             this.numericUpDown1.TabIndex = 1;
             this.numericUpDown1.Tag = "";
             this.numericUpDown1.ThousandsSeparator = true;
+            this.numericUpDown1.Value = new decimal(new int[] { 1000, 0, 0, 0 });
             // 
             // exit_btn
             // 
@@ -116,12 +117,12 @@
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { this.Column1, this.Column2, this.Column3, this.Column4, this.Column5 });
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { this.Column1, this.Column2, this.Column3, this.Column4, this.Column5, this.sorting });
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.dataGridView1.RowTemplate.Height = 32;
-            this.dataGridView1.Size = new System.Drawing.Size(801, 315);
+            this.dataGridView1.Size = new System.Drawing.Size(833, 315);
             this.dataGridView1.TabIndex = 1;
             // 
             // Column1
@@ -159,22 +160,30 @@
             this.Column5.Name = "Column5";
             this.Column5.ReadOnly = true;
             // 
+            // sorting
+            // 
+            this.sorting.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.sorting.HeaderText = "Отсортировано?";
+            this.sorting.Name = "sorting";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(832, 450);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.ShowIcon = false;
-            this.Text = "Form1";
+            this.Text = "Semkin K.M. 23VP1 12-17 lab";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn sorting;
 
         private System.Windows.Forms.DataGridViewCheckBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
--- Lab12-17/Form1.cs
+++ Lab12-17/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace Lab12_17
@@ -27,19 +21,156 @@ namespace Lab12_17
             dataGridView1.Rows[4].Cells[1].Value = "Быстрая";
             dataGridView1.Rows[5].Cells[1].Value = "Линейная";
             dataGridView1.Rows[6].Cells[1].Value = "Встроенная";
+
+            dataGridView1.Rows[0].Cells[0].Value = true;
+            dataGridView1.Rows[1].Cells[0].Value = true;
+            dataGridView1.Rows[2].Cells[0].Value = true;
+            
         }
-        
-        private void sort_btn_Click(object sender, EventArgs e)
+
+        private void table_cleaner() {
+
+
+            for (int i = 2; i < dataGridView1.Rows[0].Cells.Count; i++) {
+                dataGridView1.Rows[0].Cells[i].Value = null;
+            }
+            for (int i = 2; i < dataGridView1.Rows[1].Cells.Count; i++) {
+                dataGridView1.Rows[1].Cells[i].Value = null;
+            }
+            for (int i = 2; i < dataGridView1.Rows[2].Cells.Count; i++) {
+                dataGridView1.Rows[2].Cells[i].Value = null;
+            }
+
+        }
+
+        private void sort_btn_Click(object sender, EventArgs e) {
+            
+            table_cleaner();
+            
+            int size = (int)numericUpDown1.Value;
+            int[] array = GenerateArray(size);
+
+            // Проверяем, какие сортировки выбраны
+            if (Convert.ToBoolean(dataGridView1.Rows[0].Cells[0].Value)) // Обмен (пузырьковая)
+                RunSort(array, BubbleSort, 0);
+
+            if (Convert.ToBoolean(dataGridView1.Rows[1].Cells[0].Value)) // Выбор
+                RunSort(array, SelectionSort, 1);
+
+            if (Convert.ToBoolean(dataGridView1.Rows[2].Cells[0].Value)) // Включение
+                RunSort(array, InsertionSort, 2);
+        }
+        private void RunSort(int[] originalArray, Func<int[], (int, int, long)> sortMethod, int rowIndex)
         {
-            
-            
-            
+            int[] array = (int[])originalArray.Clone();
+            (int comparisons, int swaps, long time) = sortMethod(array);
+
+            dataGridView1.Rows[rowIndex].Cells[2].Value = comparisons;
+            dataGridView1.Rows[rowIndex].Cells[3].Value = swaps;
+            dataGridView1.Rows[rowIndex].Cells[4].Value = time;
+            dataGridView1.Rows[rowIndex].Cells[5].Value = IsSorted(array);
+        }
+
+        private int[] GenerateArray(int size)
+        {
+            Random rand = new Random();
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+                array[i] = rand.Next(1, size);
+            return array;
+        }
+
+        private (int, int, long) BubbleSort(int[] array)
+        {
+            int comparisons = 0, swaps = 0;
+            bool flag;
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < array.Length - 1; i++) {
+                
+                flag = true;
+                
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    comparisons++;
+                    if (array[j] > array[j + 1])
+                    {
+                        (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                        flag = false;
+                        swaps++;
+                        
+                    }
+                }
+
+                if (flag) {
+                    stopwatch.Stop();
+                    return (comparisons, swaps, stopwatch.ElapsedMilliseconds);
+                }
+            }
+            stopwatch.Stop();
+            return (comparisons, swaps, stopwatch.ElapsedMilliseconds);
+        }
+
+        private (int, int, long) SelectionSort(int[] array)
+        {
+            int comparisons = 0, swaps = 0;
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                
+                int minIndex = i;
+                
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    comparisons++;
+                    if (array[j] < array[minIndex])
+                        minIndex = j;
+                }
+                
+                (array[i], array[minIndex]) = (array[minIndex], array[i]);
+                swaps++;
+            }
+            stopwatch.Stop();
+            return (comparisons, swaps, stopwatch.ElapsedMilliseconds);
+        }
+        private (int, int, long) InsertionSort(int[] array)
+        {
+            int comparisons = 0, swaps = 0;
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+
+
+                while (j >= 0 && array[j] > key)
+                {
+                    comparisons++;
+                    array[j + 1] = array[j];
+                    j--;
+                    swaps++;
+                }
+                array[j + 1] = key;
+            }
+            stopwatch.Stop();
+            return (comparisons, swaps, stopwatch.ElapsedMilliseconds);
+        }
+
+        private bool IsSorted(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] > array[i])
+                    return false;
+            }
+            return true;
         }
 
         private void exit_btn_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
-        
     }
 }
